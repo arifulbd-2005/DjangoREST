03c329e72e1b9ac415a87fd4630b5b620653076e
Md Ariful Islam <123arifulislam111@gmail.com>
2026-07-13 22:26:14 +0600

    Delete data in django rest api

--- djangorest/drapi/views.py
+++ djangorest/drapi/views.py
@@ -63,3 +63,16 @@ def aiquest_create(request):
             return HttpResponse(json_data, content_type='application/json')
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type='application/json')
+   
+    if request.method == 'DELETE':
+        json_data = request.body
+        #json to stream convert
+        stream = io.BytesIO(json_data)
+        #stream to python
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+        aiq = Aiquest.objects.get(id=id)
+        aiq.delete()
+        res = {'msg':'Successfully Deleted data'}
+        json_data = JSONRenderer().render(res)
+        return HttpResponse(json_data, content_type='application/json')
