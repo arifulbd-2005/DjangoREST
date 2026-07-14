fb7b653638f721fc439775e8df1723711a23eada
Md Ariful Islam <123arifulislam111@gmail.com>
2026-07-14 19:40:45 +0600

    Create & use GET method in REST API

--- djangorest/drapi/views.py
+++ djangorest/drapi/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 from .models import Aiquest
+from.serializers import AiquestSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+
+@api_view(['GET'])
+def aiquest_create(request, pk=None):
+    if request.method == 'GET':
+        id = pk
+        #Spacific data show
+        if id is not None:
+            #compelx data
+            ai = Aiquest.objects.get(id=id)
+            #python dictionary
+            serializer = AiquestSerializer(ai)
+            return Response(serializer.data)
+        #all data show
+        #complex data
+        ai = Aiquest.objects.all()
+        #python dictionary
+        Serializer = AiquestSerializer(ai, many=True)
+        return Response(Serializer.data)
+
+
+
+
+"""from django.shortcuts import render
+from .models import Aiquest
 from .serializers import AiquestSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
@@ -75,4 +104,4 @@ def aiquest_create(request):
         aiq.delete()
         res = {'msg':'Successfully Deleted data'}
         json_data = JSONRenderer().render(res)
-        return HttpResponse(json_data, content_type='application/json')
+        return HttpResponse(json_data, content_type='application/json')"""
