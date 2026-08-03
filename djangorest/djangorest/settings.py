b0e5b619d58cfa3f91fed2d3df24778f0176bc22
Md Ariful Islam <123arifulislam111@gmail.com>
2026-08-03 16:27:28 +0600

    Create Session Authentication in Django REST Framework

--- djangorest/djangorest/settings.py
+++ djangorest/djangorest/settings.py
@@ -119,11 +119,20 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-REST_FRAMEWORK = {
+"""REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
+}"""
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ]
 }
